ba3819b2f1c1371c19eabad1cbcf22cc34644b7a
proter21 <petiotopalov@gmail.com>
2025-02-20 12:22:35 +0200

    refactor: remove loading state and associated spinner from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,6 @@ export default function Home() {
   const [registeredCompetitions, setRegisteredCompetitions] = useState<
     string[]
   >([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -77,20 +76,10 @@ export default function Home() {
           );
         }
       }
-
-      setLoading(false);
     };
 
     fetchData();
   }, []);
-
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-500"></div>
-      </div>
-    );
-  }
 
   return (
     <>
